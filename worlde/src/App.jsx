ebe5f2284c428be525152f62d9be2b93f842b869
Bakhtiar Rasheed <22100267@lums.edu.pk>
2022-09-20 18:04:26 +0500

    added api code

--- worlde/src/App.jsx
+++ worlde/src/App.jsx
@@ -1,8 +1,8 @@
-import { useState,createContext } from 'react'
+import { useState,createContext ,useEffect} from 'react'
 import './App.css'
 import Board from './components/Board'
 import Keyboard from './components/Keyboard'
-import { boardDefault } from './Words'
+import { boardDefault,generateWord } from './Words'
 
 
 export const AppContext=createContext()
@@ -12,6 +12,20 @@ export const AppContext=createContext()
 function App() {
 const [board,setBoard]=useState(boardDefault)
 const [currAttempt,setCurrAttempt]=useState({attempt:0,letterPos:0})
+const [word,setWord]=useState("")
+
+
+
+const correctWord="RIGHT"
+
+useEffect(()=>{
+  const word=generateWord().then((res)=>{
+    setWord(word)
+  })
+  
+},[])
+
+
 
 const onSelectLetter=(keyVal)=>{
   if(currAttempt.letterPos>4){return;}
@@ -29,10 +43,30 @@ newBoard[currAttempt.attempt][currAttempt.letterPos-1]="";
 setBoard(newBoard)
 setCurrAttempt({...currAttempt,letterPos:currAttempt.letterPos-1})
 }
+
+async function checkWordValid(word){
+  const res=await fetch(`https://thatwordleapi.azurewebsites.net/ask/?word=${word}`)
+  const data=await res.json()
+  return data.Response
+} 
 const onEnter=()=>{
   if(currAttempt.letterPos!==5){return;}
-               setCurrAttempt({attempt:currAttempt.attempt+1,letterPos:0}) 
-}
+
+  let currWord=""
+
+  for(let i=0;i<5;i++){
+    currWord+=board[currAttempt.attempt][i]
+  }
+  if(checkWordValid(currWord)){
+    setCurrAttempt({attempt:currAttempt.attempt+1,letterPos:0}) 
+  }
+  else{
+    alert("Not a word")
+  }
+  }
+  
+
+             
 
 
 
@@ -43,7 +77,7 @@ const onEnter=()=>{
     <nav>
       <h1>Wordle</h1>
     </nav>
-    <AppContext.Provider value={{board,setBoard,currAttempt,setCurrAttempt,onSelectLetter,onDelete,onEnter}}>
+    <AppContext.Provider value={{board,setBoard,currAttempt,setCurrAttempt,onSelectLetter,onDelete,onEnter,correctWord}}>
       <div className="game">
     <Board />
     <Keyboard />
--- worlde/src/Words.js
+++ worlde/src/Words.js
@@ -4,3 +4,17 @@ export const boardDefault = [["", "", "", "", ""],
                             ["", "", "", "", ""],
                             ["", "", "", "", ""],
                             ["", "", "", "", ""]]
+
+
+export const generateWord=async()=>{
+    let wordSet;
+    try{
+   const res= await fetch("https://thatwordleapi.azurewebsites.net/get/")
+   const data=await res.json()
+   return data.Response
+
+    }
+    catch(err){
+        console.log(err)
+    }
+}
